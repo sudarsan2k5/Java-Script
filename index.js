f31032ebca27067a257a9c273a9fc7abd83b6cb1
Sudarsan Mansingh <108286640+sudarsan2k5@users.noreply.github.com>
2022-07-16 14:58:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("This is my first video i.e java Script")
+var a = 133 // a contains 133
+console.log(a)
+a = "sudarsan"
+console.log(a)
+// let 4sudarsan = 4 // not allowed this will throw error
+// var = 5343--->this is also not allowed,
