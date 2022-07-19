316a0eb0b83333c2be9c25ca6e1b738e5df45b8e
Sudarsan Mansingh <108286640+sudarsan2k5@users.noreply.github.com>
2022-07-19 00:32:18 +0000

    const,let and var

---
+++ const,let and var.js
@@ -0,0 +1,11 @@
+console.log("const,var and let in java script")
+// var a = 12;
+// var a = 123;
+let b = "sudarsan"
+const author = "sudarsan"
+author = 12 // Throw an error because constant cannot be changed
+const sudarsan = 0;
+{
+    let b = 'this'
+    console.log(b)
+}
--- index.js
+++ index.js
@@ -5,3 +5,15 @@ a = "sudarsan"
 console.log(a)
 // let 4sudarsan = 4 // not allowed this will throw error
 // var = 5343--->this is also not allowed,
+console.log("const,var and let in java script")
+// var a = 12;
+// var a = 123;
+let b = "sudarsan"
+const author = "sudarsan"
+//author = 12 // Throw an error because constant cannot be changed
+const sudarsan = 0;
+{
+    let b = 'this'
+    console.log(b)
+}
+console.log(b)
