1c4006f2e3b11c2e7d7f77b5c74ba8f562457735
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2022-07-20 06:15:36 +0530

    primitive and object

--- const_let_var.js
+++ const_let_var.js
@@ -3,7 +3,7 @@ console.log("const,var and let in java script")
 // var a = 123;
 let b = "sudarsan"
 const author = "sudarsan"
-author = 12 // Throw an error because constant cannot be changed
+// author = 12 // Throw an error because constant cannot be changed
 const sudarsan = 0;
 {
     let b = 'this'
