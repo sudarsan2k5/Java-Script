cad64741d0e94eba861726eda357813d563a17fa
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2022-07-21 20:25:43 +0530

    js question

---
+++ js_question.js
@@ -0,0 +1,30 @@
+// Chapter 1 - Q1
+ let a = "sudarsan"
+ let b = 6
+ console.log(a + b)
+
+// Chapter 1 - Q2
+ console.log(typeof (a+b))
+
+ // Chapter 1 - Q3
+ const a1 = {
+    name: "sudarsan",
+    section: 1,
+   }
+// Chapter 1 -Q4
+   a1['frind'] = "shubham"
+   a1['name'] = "lovish"
+ console.log(a1)
+ // Chapter 1 - Q5
+
+ const dict = {
+    symbol: "a mark or character used as a conventional",
+    "check out": "settle one's hotel bill before leaving.",
+    deploy: "bring into effective action."
+ }
+
+ console.log(dict['symbol'])
+
+ 
+
+
--- primitives_object.js
+++ primitives_object.js
@@ -2,18 +2,17 @@
 let a = null;
 let b = 345;
 let c = true; // can also be false
-let d = BigInt("577") + BigInt("3")
-let e = "sudarsan"
-let f = Symbol("This is a symble")
-let g = undefined
-console.log(a, b, c, d, d, e, f, g)
+let d = BigInt("577") + BigInt("3");
+let e = "sudarsan";
+let f = Symbol("This is a symble");
+let g = undefined;
+console.log(a, b, c, d, d, e, f, g);
 console.log(typeof e)  // ->This is represent which type of primitive,
 
 // Object in Js
-const item = {
-    "sudarsan": true,
-    "ragu": false,
-    "harry": 56,
-    "ronal": undefined
-}
-console.log(item["ragu"])
+const item = {"sudarsan": true, 
+                "ragu": false, 
+                "harry": 56, 
+                "ronal": undefined
+            };
+console.log(item["ragu"]);
